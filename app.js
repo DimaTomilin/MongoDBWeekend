126168cb71dd2be0f661b8796583ad459e7d2d84
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2021-11-11 14:11:10 +0200

    Config template

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const jsonParser = bodyParser.json();
 const app = express();
 require("dotenv").config();
+const morgan = require("morgan");
+
 const userRouter = require("./routers/user");
-const { morganBodyLogger } = require("./morgan");
+
 const errorHandlingMiddleware = require("./middlewares/errorHandlingMiddleware");
 const unknownEndpoint = require("./middlewares/unknownEndpoint");
 
-const port = process.env.PORT || 3000;
+app.use(express.json())
 
-const mongo = process.env.DATABASE;
+morgan.token('body', function (req, res) {
+    return JSON.stringify(req.body);
+  });
+  app.use(
+    morgan(' :method :url :status :res[content-length] - :response-time ms :body')
+  );
 
 //connecting to database
 mongoose
-    .connect(mongo)
+    .connect(process.env.DATABASE)
     .then((result) => {
         console.log("connected to MongoDB");
     })
     .catch((error) => {
         console.log("error connecting to MongoDB:", error.message);
     });
-
-app.use(morganBodyLogger);
 
 app.get("/", (req, res) => {
     res.send("working");
@@ -36,6 +39,8 @@ app.use(unknownEndpoint);
 // error handling middleware
 app.use(errorHandlingMiddleware);
 
+
+const port = process.env.PORT || 3030;
 app.listen(port, () => {
     console.log(`litsening in port ${port}`);
 });
